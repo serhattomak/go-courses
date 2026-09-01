64ccfbab7f8b012119110e70f1b8d13d591be304
Serhat Tomak <serhattomak@gmail.com>
2026-09-01 12:42:53 +0300

    feat(core): add inflation adjustment to investment calculation
    Introduce an inflation rate constant and calculate the future real
    value of the investment to account for purchasing power changes over
    time.

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,11 +6,14 @@ import (
 )
 
 func main() {
+	const inflationRate = 2.5
 	var investmentAmount float64 = 1000
 	years := 10.0
 	expectedReturnRate := 5.5
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, float64(years))
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
